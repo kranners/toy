25324068a14e4ef0f5cc0c0a71b5d654ca1ce9fc
Aaron Pierce <aaron@cute.engineer>
2025-01-20 10:13:38 +1100

    build(test): installing vitest browser

--- src/systems/tests/base.test.ts
+++ src/systems/tests/base.test.ts
@@ -1,20 +1,14 @@
 import { Sky } from "three/examples/jsm/Addons";
-import { Engine, GRAVITY, queryEntity } from "../..";
+import { Engine, queryEntity } from "../..";
 import { base, isInteractive } from "../base";
 import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { describe, expect, it } from "vitest";
 
-let engine: Engine;
-
-beforeAll(async () => {
-  const renderer = new WebGLRenderer();
-  const scene = new Scene();
-  const camera = new PerspectiveCamera();
-
-  const rapier = await import("@dimforge/rapier3d");
-  const world = new rapier.World(GRAVITY);
-
-  engine = { renderer, scene, camera, world };
-});
+const renderer = new WebGLRenderer();
+const scene = new Scene();
+const camera = new PerspectiveCamera();
+const world = {};
+const engine = { renderer, scene, camera, world } as Engine;
 
 describe("base system", () => {
   describe("given a state with a single object3d", () => {
